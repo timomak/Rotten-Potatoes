db291505a380ab53226ae5279891f16b1fb9e37a
timomak <tmakhlay2@gmail.com>
2018-01-16 11:16:30 -0800

    Working reviews

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const app = express()
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/rotten-potatoes', { useMongoClient: true });
-
-
+const bodyParser = require('body-parser');
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  movieTitle: String,
+  description: String
 });
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.get('/', (req, res) => {
   Review.find().then((reviews) => {
@@ -24,6 +36,9 @@ app.get('/', (req, res) => {
 // INDEX
 app.get('/reviews', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
+})
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
 })
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
