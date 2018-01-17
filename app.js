89ad9fa209aa2fa9a9e1a012243d21ba96e80934
timomak <tmakhlay2@gmail.com>
2018-01-17 13:12:06 -0800

    Design Update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -11,7 +12,7 @@ const Review = mongoose.model('Review', {
 });
 
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(methodOverride('_method'))
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
     console.log(err);
   })
 })
+
 // let reviews = [
 //   { title: "Great Review" },
 //   { title: "Next Review" }
@@ -35,10 +37,42 @@ app.get('/', (req, res) => {
 
 // INDEX
 app.get('/reviews', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect('/reviews/' + review._id)
+  }).catch((err) => {
+    console.log(err.message)
+  })
 })
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+})
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body).then((review) => {
+    res.redirect('/reviews/'+ review._id)
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+app.delete('/reviews/:id', function(req,res){
+  console.log("Delete Review")
+  Review.findByIdAndRemove(req.params.id).then((review) =>{
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
