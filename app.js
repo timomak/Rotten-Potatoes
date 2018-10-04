5b59630e56fb4383e2b8e2a0a83b4b660d360940
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-04 16:40:39 -0700

    Working going to id

--- app.js
+++ app.js
@@ -28,16 +28,25 @@ app.get('/', (req, res) => {
 // Create reviews
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
 // New reviews
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+
+// Show review
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(3000, () => {
