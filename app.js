f6c97991633464d4fceae6db9a06c178abae3d94
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-04 16:09:13 -0700

    Code before MongoDB

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const app = express()
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('reviews-index', { reviews: reviews });
 })
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
