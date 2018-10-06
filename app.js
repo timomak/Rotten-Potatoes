a48f4e0bd5816f4c946e64d4371bcfe254c0251a
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-06 16:44:26 -0700

    Heroku not working

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var exphbs = require('express-handlebars');
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', {useMongoClient: true});
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes-tm');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/rotten-potatoes', {useMongoClient: true});
 const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 app.use(methodOverride('_method'))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,9 +17,11 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 const reviews = require('./controllers/reviews')(app);
 
-app.listen(27017, () => {
-  console.log('App listening on port 27017!')
-})
+
+
+// app.listen(3000, () => {
+//   console.log('App listening on port 3000!')
+// })
 
 // For Chai
 module.exports = app;
