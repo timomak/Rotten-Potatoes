c828cd13bf5b5cee35322ef6ff36389d79875a15
Timofey Makhlay <tmakhlay2@gmail.com>
2018-10-10 16:43:12 -0700

    Finished Everything

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,5 +8,13 @@ module.exports = (app) => {
       console.log(err.message);
     });
   });
-
+  // DELETE
+  app.delete('/reviews/comments/:id', function (req, res) {
+    console.log("DELETE comment")
+    Comment.findByIdAndRemove(req.params.id).then((comment) => {
+      res.redirect(`/reviews/${comment.reviewId}`);
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
 }
